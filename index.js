6729cf23e9e85be09a2a1a22cf6cda43eb5907dd
tusharpatel777 <tussipatel826@gmail.com>
2025-03-28 18:47:29 +0530

    okay

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js"
 import taskRoutes from "./routes/taskRoutes.js";
 
+dotenv.config();
+
 const app = express();
 app.use(express.json());
-app.use(cors()); 
+app.use(cors({ origin: "*" }));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
